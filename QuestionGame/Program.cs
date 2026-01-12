723eae06feebf880304e8bea53d23c774c55894e
Mcfluffelbl <linnea.jmb@gmail.com>
2026-01-12 13:42:57 +0100

    First commit, added intro text and map with todo

--- QuestionGame/Program.cs
+++ QuestionGame/Program.cs
@@ -1,21 +1,29 @@
 ﻿
 Console.Title = "Question Game";
 
+// Introduction
 Console.WriteLine("Welcome to the Question Game!");
-Console.WriteLine("You will be asked a series of questions. Try to answer them correctly!");
-Console.WriteLine("You can choose in betwenn 5 diffrent themes");
+Console.WriteLine("\nYou will be asked a series of questions. Try to answer them correctly!");
+Console.WriteLine("\nYou can choose in betwenn 5 diffrent themes");
 Console.WriteLine("Themes: Science, History, Geography, Animals, Entertainment");
-Console.WriteLine("Rules:");
-Console.WriteLine("Each theme contains 10 questions, randomly sorted out.");
-Console.WriteLine("You will have 4 options for each question.");
-Console.WriteLine("For each correct answer, you will earn points.");
-Console.WriteLine("You can also choose to answer questions from all themes mixed together.");
+Console.WriteLine("\nRules:");
+Console.WriteLine("1. Each theme contains 10 questions, randomly sorted out.");
+Console.WriteLine("2. You will have 4 options for each question.");
+Console.WriteLine("3. For each correct answer, you will earn points.");
+Console.WriteLine("4. You can also choose to answer questions from all themes mixed together.");
 
-Console.WriteLine("Press any button to start...");
-Console.ReadKey();
+// Wait for user to be ready
+Console.WriteLine("\nPress any button to start...");
+Console.ReadKey(true);
 Console.WriteLine();
 
+// Initialize score and question count
 var score = 0;
 var askedQuestions = 0;
 
+// Main game loop
 
+// End of game summary
+Console.WriteLine($"\nGame Over! You answered {askedQuestions} questions with a total score of {score} points.");
+Console.WriteLine("Tryck på valfri knapp för att avsluta spelet...");
+Console.ReadKey(true);
